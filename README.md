ffb46fc71f782263ff641c1d1dbb7c37e6b5bbb0
Nick-Hericks <nickhericks@gmail.com>
2019-02-19 17:11:09 +0200

    Build next button

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Carousel component
-> A carousel/slider component that allows for content to be shuffled through either at a set interval or actively by the user.
+> A carousel component that allows for content to be shuffled through either at a set interval or actively by the user.
 
 ## View project
  :mag: Live version available at [nickhericks.github.io/carousel/](https://nickhericks.github.io/carousel/)
+
+## What I learned
+ -
+
+```javascript
+
+const slideWidth = slides[0].getBoundingClientRect().width
+
+
+```
 
 ## Acknowledgements
 This project is part of a great JS course called [Learn JavaScript](https://learnjavascript.today/). Thank you to the course creator [@zellwk](https://github.com/zellwk) :raised_hands:
--- js/main.js
+++ js/main.js
@@ -1,2 +1,40 @@
-// # START EDITING YOUR JAVASCRIPT HERE
-// ===============
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+// Get width of first slides
+const slideWidth = slides[0].getBoundingClientRect().width;
+const nextButton = document.querySelector('.jsNext');
+const dotsContainer = document.querySelector('.carousel__nav');
+
+// Give each slide proper positioning based on slide width
+slides.forEach((slide, index) => {
+  slide.style.left = slideWidth * index + 'px';
+});
+
+
+
+
+nextButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.is-selected');
+  const nextSlide = currentSlide.nextElementSibling;
+  const amountToMove = nextSlide.style.left;
+  const isFinalSlide = !nextSlide.nextElementSibling;
+  const currentDot = dotsContainer.querySelector('.is-selected');
+  const nextDot = currentDot.nextElementSibling;
+
+  track.style.left = '-' + amountToMove;
+
+  currentSlide.classList.remove('is-selected');
+  nextSlide.classList.add('is-selected');
+
+  if (isFinalSlide) {
+    nextButton.classList.add('is-hidden');
+  }
+
+
+
+  currentDot.classList.remove('is-selected');
+  nextDot.classList.add('is-selected');
+
+
+
+});
